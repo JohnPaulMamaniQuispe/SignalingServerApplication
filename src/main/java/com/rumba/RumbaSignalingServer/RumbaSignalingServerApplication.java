2570d3155258623dab3e6bf886f9fe28a3d3c826
JohnPaulM@m@ni <u201919105@upc.edu.pe>
2024-09-04 12:33:39 -0500

    server

---
+++ src/main/java/com/rumba/RumbaSignalingServer/RumbaSignalingServerApplication.java
@@ -0,0 +1,13 @@
+package com.rumba.RumbaSignalingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumbaSignalingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumbaSignalingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rumba/RumbaSignalingServer/RumbaSignalingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumba.RumbaSignalingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumbaSignalingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
